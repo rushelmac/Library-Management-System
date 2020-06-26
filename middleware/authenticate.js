d6bb11ec5ed2afa9818efb437117872450f7f5ca
Rushikesh Shelke <58942280+rushelmac@users.noreply.github.com>
2020-06-26 17:21:11 +0530

    Commented unused code

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,81 +1,81 @@
 const Users = require('../models/user');
 const Admins = require('../models/admin');
-const passport = require('passport');
+// const passport = require('passport');
 const bcrypt = require("bcrypt");
 
-const LocalStrategy = require('passport-local').Strategy;
+// const LocalStrategy = require('passport-local').Strategy;
 
-passport.use('User', new LocalStrategy(
-    (usernm, hash, done) => {
-        Users.findOne({userName : usernm} , (err,user) => {
-            if(err){
-                return done(err);
-            }
-            else if(!user){
-                return done(null, false, {message: "Invalid Username"})
-            }
-            else{
-                bcrypt.compare(hash,user.password,(err,result)=>{
-                    if(result == true){
-                        return done(null,user);
-                    }
-                    else{
-                        return done(null,false,{message:"Incorrect password"});
-                    }
-                })
-            }
-        });
-    }
-));
+// passport.use('User', new LocalStrategy(
+//     (usernm, hash, done) => {
+//         Users.findOne({userName : usernm} , (err,user) => {
+//             if(err){
+//                 return done(err);
+//             }
+//             else if(!user){
+//                 return done(null, false, {message: "Invalid Username"})
+//             }
+//             else{
+//                 bcrypt.compare(hash,user.password,(err,result)=>{
+//                     if(result == true){
+//                         return done(null,user);
+//                     }
+//                     else{
+//                         return done(null,false,{message:"Incorrect password"});
+//                     }
+//                 })
+//             }
+//         });
+//     }
+// ));
 
-passport.serializeUser(function (user1, done) {
-    console.log("IN SERIALIZER")
-    const obj = {id:user1._id,username:user1.username,isAdmin:user1.isAdmin};
-    done(null,obj);
-});
+// passport.serializeUser(function (user1, done) {
+//     console.log("IN SERIALIZER")
+//     const obj = {id:user1._id,username:user1.username,isAdmin:user1.isAdmin};
+//     done(null,obj);
+// });
 
-passport.deserializeUser(function(object, done) {
-    Users.find({username : object.user}, function(err, founduser) {
-        done(err, founduser);
-    });
-});
+// passport.deserializeUser(function(object, done) {
+//     Users.find({username : object.user}, function(err, founduser) {
+//         done(err, founduser);
+//     });
+// });
 
-passport.use('Admin', new LocalStrategy(
-    (usernm, hash, done) => {
-        Admins.findOne({username : usernm} , (err,user) => {
-            if(err){
-                return done(err);
-            }
-            else if(!user){
-                return done(null, false, {message: "Invalid Username"})
-            }
-            else{
-                bcrypt.compare(hash,user.password,(err,result)=>{
-                    if(result == true){
-                        return done(null,user);
-                    }
-                    else{
-                        return done(null,false,{message:"Incorrect password"});
-                    }
-                })
-            }
-        });
-    }
-));
+// passport.use('Admin', new LocalStrategy(
+//     (usernm, hash, done) => {
+//         Admins.findOne({username : usernm} , (err,user) => {
+//             if(err){
+//                 return done(err);
+//             }
+//             else if(!user){
+//                 return done(null, false, {message: "Invalid Username"})
+//             }
+//             else{
+//                 bcrypt.compare(hash,user.password,(err,result)=>{
+//                     if(result == true){
+//                         return done(null,user);
+//                     }
+//                     else{
+//                         return done(null,false,{message:"Incorrect password"});
+//                     }
+//                 })
+//             }
+//         });
+//     }
+// ));
 
-passport.serializeUser(function (user1, done) {
-    const obj = {id:user1._id,user:user1.username,isAdmin:user1.isAdmin};
-    done(null,obj);
-});
+// passport.serializeUser(function (user1, done) {
+//     const obj = {id:user1._id,user:user1.username,isAdmin:user1.isAdmin};
+//     done(null,obj);
+// });
 
-passport.deserializeUser(function(object, done) {
-    Admins.find({username : object.user}, function(err, user1) {
-        done(err, user1);
-    });
-});
+// passport.deserializeUser(function(object, done) {
+//     Admins.find({username : object.user}, function(err, user1) {
+//         done(err, user1);
+//     });
+// });
 
-module.exports.userAuth = passport.authenticate("User");
-module.exports.adminAuth = passport.authenticate("Admin");
+// module.exports.userAuth = passport.authenticate("User");
+// module.exports.adminAuth = passport.authenticate("Admin");
 
 // check if logged user is admin before performing some operations
 var verifyAdmin = (req,res,next) => {
